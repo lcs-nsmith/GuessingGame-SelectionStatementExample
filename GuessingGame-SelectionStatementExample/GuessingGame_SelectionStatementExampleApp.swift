5df17c385e86b7a20959919eaf6927d85ad35512
Nathan Smith <nsmith@lcs.on.ca>
2021-12-02 09:10:14 -0500

    Initial Commit

---
+++ GuessingGame-SelectionStatementExample/GuessingGame_SelectionStatementExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGame_SelectionStatementExampleApp.swift
+//  GuessingGame-SelectionStatementExample
+//
+//  Created by Nate S on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGame_SelectionStatementExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
